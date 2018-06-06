08fbcff6022fa1401c4d3f6cdf6019a1426361e1
connorstroomberg <ConnorStroomberg@users.noreply.github.com>
2018-06-06 15:21:44 +0200

    setup build for app manager

--- config/index.js
+++ config/index.js
@@ -90,7 +90,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '/apps/${app.id}',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
